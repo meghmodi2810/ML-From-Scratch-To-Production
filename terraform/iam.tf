cdecb910136b7780c88d55ac16d0ba71e0a8ae02
Megh <23bmiit146@gmail.com>
2026-08-21 12:46:14 +0530

    fix(cd): update OIDC audience and session parameters for AWS STS authentication

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -110,7 +110,7 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 # ---------------------------------------------------------------------
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url            = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
+  client_id_list = ["sts.amazonaws.com", "https://github.com/meghmodi2810"]
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
@@ -142,7 +142,8 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:${var.github_repository}:*",
-        "repo:${var.github_repository}:ref:refs/heads/*"
+        "repo:${lower(var.github_repository)}:*",
+        "repo:*:ref:refs/heads/main"
       ]
     }
   }
